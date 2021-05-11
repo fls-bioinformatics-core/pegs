dd5eb3df63b65a60748736cf7dba0dc36e8ffd1a
Peter Briggs <peter.briggs@manchester.ac.uk>
2021-05-11 16:00:43 +0100

    setup.py: add Python 3.8 as supported version in metadata.

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         "Programming Language :: Python :: 3",
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     test_suite='nose.collector',
     tests_require=['nose'],
