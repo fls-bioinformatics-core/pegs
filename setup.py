0ff02d6c71b568d573ade19ec506d6460a7611ce
Peter Briggs <peter.briggs@manchester.ac.uk>
2021-05-12 14:25:15 +0100

    setup.py: drop Python 2/2.7 from the supported Python versions in metadata.

--- setup.py
+++ setup.py
@@ -60,8 +60,6 @@ setup(
         'Operating System :: POSIX',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         "Programming Language :: Python :: 3",
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
