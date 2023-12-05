48e0d5b4b5a3e50d8127eba8de63ab41349efc92
Peter Briggs <peter.briggs@manchester.ac.uk>
2023-12-05 16:54:32 +0000

    Pin the required version of 'xlsxwriter' to 3.9.1.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = ['numpy==1.19.5',
                     'matplotlib==3.3.4',
                     'pillow==8.1.1',
                     'seaborn==0.11.1',
-                    'xlsxwriter >= 0.8.4',
+                    'xlsxwriter==3.9.1',
                     'pathlib2']
 
 # Acquire package version for installation
